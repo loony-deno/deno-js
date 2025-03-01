eaf0a36ed4f1233c7dfd00b4d7403382188a7e35
Sankar Boro <sankar.boro@yahoo.com>
2025-03-01 07:48:39 +0530

    deno and PG

--- http/main.ts
+++ http/main.ts
@@ -1,16 +1,20 @@
 import { Application } from "jsr:@oak/oak/application";
 import { Router } from "jsr:@oak/oak/router";
+import { client } from "./pg.ts";
 
 const router = new Router();
-router.get("/", (ctx) => {
-  ctx.response.body = `<!DOCTYPE html>
-    <html>
-      <head><title>Hello oak!</title><head>
-      <body>
-        <h1>Hello oak!</h1>
-      </body>
-    </html>
-  `;
+router.get("/", async (ctx) => {
+  await client.connect();
+  const result = await client.queryArray(
+    "SELECT uid, fname, lname, email FROM users"
+  );
+  await client.end();
+  ctx.response.body = result.rows.map((user: any) => ({
+    user_id: user[0],
+    fname: user[1],
+    lname: user[2],
+    email: user[3],
+  }));
 });
 
 const app = new Application();
